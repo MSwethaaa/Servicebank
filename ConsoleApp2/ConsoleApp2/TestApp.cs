a354c5bf52b976d19fa4a30c3a14a87d440c907d
DELL <Swetha>
2024-01-04 15:32:57 +0530

    Add project files.

---
+++ ConsoleApp2/ConsoleApp2/TestApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace TestApp
+{
+    static class TestExt
+    {
+        public static void ExtTapCard(this TestApp t)
+        {
+            Console.WriteLine("this is extention method");
+        }
+    }
+    internal class TestApp
+    {
+        static void Main(string[] args)
+        {
+            // Existing features
+            College college = new College();
+            college.Tenth();
+            college.plus2();
+            college.Graduation();
+            college.postgradution()
+
+           
+        }
+    }
+}
+
